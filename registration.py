ee2f65b54d3deadd2bfebfa53bf32e857a748907
Mahalakshmi.N <mahalakshmin285@gmail.com>
2025-12-09 16:33:48 +0530

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,27 @@
+name=["maha","lakshmi","Charlie","David","Emma","Francis","Gokul"]
+reg_no=[1501,1502,1503,1504,1505,1506,1507]
+password=[111,112,113,114,115,116,117,118]
+def registration(name, reg_no, password):
+    rno = int(input("\n ENTER YOUR REGISTER NUMBER: "))
+    if rno in reg_no:
+        i = reg_no.index(rno)
+        psw = int(input("ENTER THE PASSWORD: "))
+        if psw == password[i]:
+            print("WELCOME", name[i])
+        else:
+            print("INCORRECT PASSWORD")
+    else:
+        print("INVALID USER ID")
+        a = int(input("TO CREATE NEW ACCOUNT PRESS 1: "))
+        if a == 1:
+            n = input("ENTER YOUR NAME: ")
+            r = int(input("CREATE YOUR REG NO: "))
+            p = int(input("CREATE YOUR PASSWORD: "))
+            name.append(n)
+            reg_no.append(r)
+            password.append(p)
+            print("ACCOUNT CREATED SUCCESSFULLY!")
+            registration(name, reg_no, password)
+        else:
+            print("EXIT")
+registration(name, reg_no, password)
